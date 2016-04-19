fdc50949eab7d7e4c73af1fa136b2e3750a31b87
KevinBreurken <bioniclefreak_19@hotmail.com>
2016-04-19 16:19:01 +0200

    Push after end of school

--- ProeveVanBekwaamheid/Assets/Scripts/UI/GameUIState.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/UI/GameUIState.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using BaseFrame.QStates;
+using BaseFrame.QUI;
 using BaseFrame.QEffect;
 using DG.Tweening;
 
@@ -8,10 +9,23 @@ namespace Base.UI {
 
     public class GameUIState : BaseUIState {
 
+		public QUIButton pauseButton;
+
+		void Awake () {
+
+			pauseButton.onClicked += PauseButton_onClicked;
+
+		}
+
         public override void Enter () {
 
             base.Enter();
 
+        }
+
+        void PauseButton_onClicked ()
+        {
+			
         }
 
         public override IEnumerator Exit () {
