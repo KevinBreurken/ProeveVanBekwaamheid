5622dde042f520836641b665643a9b91bf4d1ba1
kimberly de Jong <kimberly de Jong>
2016-04-19 14:00:44 +0200

    Boat has a better feel

--- ProeveVanBekwaamheid/Assets/Scripts/Player/PlayerBehaviour.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,18 +4,22 @@ using Chanisco;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    private float speed = 0.1f;
-    public Direction ownDirection;
+    private float speed     = 0.01f;
+    private float maxSpeed  = 0.5f;
+    private float minSpeed  = -0.5f;
+    
     private Vector2 originalPos;
+
     public HookBehaviour redHook;
     public HookBehaviour greenHook;
     public HookBehaviour blueHook;
 
     private VarsController varscontroller;
+    private SeaController seaController;
     private Area fishArea;
 
-    private SeaController seaController;
     private float Xpos;
+    private float borderPos;
 
     private void Start()
     {
@@ -27,42 +31,27 @@ public class PlayerBehaviour : MonoBehaviour {
 
     public void Update ()
     {
-        Debug.Log(Xpos);
         Controlls();
     }
 
+    /// <summary>
+    /// All scripts that are called that are referenced to controll
+    /// </summary>
     private void Controlls()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if(transform.localPosition.x > fishArea.xLeft)
-            {
-                Xpos = ChaniscoLib.AddWithMax(-Xpos,speed,1,0);
-            }
-            else
-            {
-                transform.localPosition = new Vector2(fishArea.xLeft, originalPos.y);
-                seaController.MoveSea(speed);
-            }
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (transform.localPosition.x < fishArea.xRight)
-            {
-                   Xpos = ChaniscoLib.AddWithMax(Xpos, speed, 1, 0);
-            }
-            else
-            {
-                transform.localPosition = new Vector2(fishArea.xRight, originalPos.y);
-                seaController.MoveSea(-speed);
-            }
-        }
-
+        HookControlls();
+        BoatMovementControlls();
+    }
+    /// <summary>
+    /// the calls to the Hookbehaviours to let the hooks go loose
+    /// </summary>
+    private void HookControlls()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             redHook.ReleaseHook();
         }
-        else if(Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             greenHook.ReleaseHook();
         }
@@ -71,9 +60,55 @@ public class PlayerBehaviour : MonoBehaviour {
             blueHook.ReleaseHook();
         }
     }
-
-    private void BoatMovement()
+    /// <summary>
+    /// Movement that the boat uses to move around the sea
+    /// </summary>
+    private void BoatMovementControlls()
     {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.localPosition.x > fishArea.xLeft)
+            {
+                Xpos = ChaniscoLib.AddWithMax(Xpos, -speed, maxSpeed, minSpeed);
+                borderPos = Xpos;
+            }
+            else
+            {
+                Xpos = 0;
+                transform.localPosition = new Vector2(fishArea.xLeft, originalPos.y);
+                seaController.MoveSea(-borderPos);
+                borderPos = ChaniscoLib.AddWithMax(borderPos, -speed, maxSpeed, minSpeed);
+            }
+        }
 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.localPosition.x < fishArea.xRight)
+            {
+                Xpos = ChaniscoLib.AddWithMax(Xpos, speed, maxSpeed, minSpeed);
+                borderPos = Xpos;
+            }
+            else
+            {
+                Xpos = 0;
+                transform.localPosition = new Vector2(fishArea.xRight, originalPos.y);
+                seaController.MoveSea(-borderPos);
+                borderPos = ChaniscoLib.AddWithMax(borderPos, speed, maxSpeed, minSpeed);
+            }
+        }
+        if (transform.localPosition.x >= fishArea.xRight)
+        {
+            transform.localPosition = new Vector2(fishArea.xRight, originalPos.y);
+            seaController.MoveSea(-borderPos);
+        }
+        else if (transform.localPosition.x <= fishArea.xLeft)
+        {
+            transform.localPosition = new Vector2(fishArea.xLeft, originalPos.y);
+            seaController.MoveSea(-borderPos);
+        }
+        Xpos = Mathf.SmoothStep(Xpos, 0, 0.2f);
+        borderPos = Mathf.SmoothStep(borderPos, 0, 0.25f);
+        
+        transform.Translate(Xpos,0,0);
     }
 }
