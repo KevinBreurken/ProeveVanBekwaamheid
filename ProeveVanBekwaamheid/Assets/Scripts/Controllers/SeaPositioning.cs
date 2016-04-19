f728b99d85fc6866e8777fbea0b70cf1fbb035d7
kimberly de Jong <kimberly de Jong>
2016-04-19 11:01:19 +0200

    Sea Controlls Added

---
+++ ProeveVanBekwaamheid/Assets/Scripts/Controllers/SeaPositioning.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaPositioning : MonoBehaviour {
+    private VarsController varsController;
+    private Area seaArea;
+
+    void Start()
+    {
+        varsController = VarsController.Instance;
+        seaArea = varsController.SeaField;
+    }
+
+    public void MovePosition(float Xpos)
+    {
+        Vector2 ownPosition = transform.localPosition;
+        if (Xpos < 0)
+        {
+            if (ownPosition.x <= seaArea.xLeft)
+            {
+
+            }
+            else
+            {
+                transform.Translate(new Vector2(Xpos, 0));
+
+            }
+        }
+        else
+        {
+            if (ownPosition.x >= seaArea.xRight)
+            {
+
+            }
+            else
+            {
+                transform.Translate(new Vector2(Xpos, 0));
+
+            }
+        }
+    }
+}
