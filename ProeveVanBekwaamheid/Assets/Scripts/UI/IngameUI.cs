2284a02148d7a4e8c03661870ac8a3c38237efa8
kimberly de Jong <kimberly de Jong>
2016-04-21 12:40:54 +0200

    Added Ingame UI

---
+++ ProeveVanBekwaamheid/Assets/Scripts/UI/IngameUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IngameUI : MonoBehaviour {
+
+    public Text scoreText;
+    public Text timeText;
+
+    private VarsController varsController;
+
+    void Start()
+    {
+        varsController = VarsController.Instance;
+    }
+    void Update()
+    {
+        UIElements();
+    }
+
+    void UIElements()
+    {
+        scoreText.text = varsController.GetScoreInString();
+        timeText.text = varsController.GetTimeInString();
+    }
+    
+}
