998ab0b81e8f1bda6aeb898148495aa23fad1726
KevinBreurken <bioniclefreak_19@hotmail.com>
2016-05-11 14:07:32 +0200

    Added Underwater effect.

---
+++ ProeveVanBekwaamheid/Assets/Scripts/TargetFollower.cs
@@ -0,0 +1,16 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+
+public class TargetFollower : MonoBehaviour {
+
+	public GameObject target;
+
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = new Vector3(target.transform.position.x,transform.position.y,transform.position.z);
+
+	}
+
+}
