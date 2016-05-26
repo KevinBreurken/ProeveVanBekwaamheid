51841469f4f3178f1f6a0b9db736a821ab9b9309
KevinBreurken <bioniclefreak_19@hotmail.com>
2016-05-26 11:54:03 +0200

    Applied code conventions to Hook related scripts.

--- ProeveVanBekwaamheid/Assets/Scripts/Hook/BlueHook.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Hook/BlueHook.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BlueHook : HookBehaviour {
-    public LineRenderer Chain;
-    private Transform ChainHolder;
+namespace Base.Game.Hooks {
 
-    void Start()
-    {
-        ChainHolder = Chain.transform;
-        hookStart();
-    }
+	public class BlueHook : HookBehaviour {
+		
+	    public LineRenderer Chain;
+	    private Transform ChainHolder;
 
-    void Update()
-    {
-        Chain.SetPosition(0, new Vector3(ChainHolder.position.x, ChainHolder.position.y, -0.1f));
-        Chain.SetPosition(1, new Vector3(transform.position.x, transform.position.y + 0.2f, -0.1f));
-        hookUpdate();
-    }
+	    void Start() {
+			
+	        ChainHolder = Chain.transform;
+	        hookStart();
 
-    public override void SetType()
-    {
-        ownHookColor = HookColors.YELLOW;
-        base.SetType();
-    }
+	    }
+
+	    void Update() {
+			
+	        Chain.SetPosition(0, new Vector3(ChainHolder.position.x, ChainHolder.position.y, -0.1f));
+	        Chain.SetPosition(1, new Vector3(transform.position.x, transform.position.y + 0.2f, -0.1f));
+	        hookUpdate();
+
+	    }
+
+		/// <summary>
+		/// See BaseClass function.
+		/// </summary>
+	    public override void SetType() {
+			
+	        ownHookColor = HookColors.YELLOW;
+	        base.SetType();
+
+	    }
+
+	}
 
 }
--- ProeveVanBekwaamheid/Assets/Scripts/Hook/GreenHook.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Hook/GreenHook.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GreenHook : HookBehaviour {
-    public LineRenderer Chain;
-    private Transform ChainHolder;
+namespace Base.Game.Hooks {
+		
+	public class GreenHook : HookBehaviour {
+		
+	    public LineRenderer Chain;
+	    private Transform ChainHolder;
 
-    void Start()
-    {
-        ChainHolder = Chain.transform;
-        hookStart();
-    }
+	    void Start() {
+			
+	        ChainHolder = Chain.transform;
+	        hookStart();
 
-    void Update()
-    {
-        Chain.SetPosition(0, new Vector3(ChainHolder.position.x, ChainHolder.position.y, -0.1f));
-        Chain.SetPosition(1, new Vector3(transform.position.x,transform.position.y + 0.2f, -0.1f));
-        hookUpdate();
-    }
+	    }
 
-    public override void SetType()
-    {
-        ownHookColor = HookColors.GREEN;
-        base.SetType();
-    }
+	    void Update() {
+			
+	        Chain.SetPosition(0, new Vector3(ChainHolder.position.x, ChainHolder.position.y, -0.1f));
+	        Chain.SetPosition(1, new Vector3(transform.position.x,transform.position.y + 0.2f, -0.1f));
+	        hookUpdate();
+
+	    }
+
+		/// <summary>
+		/// See BaseClass function.
+		/// </summary>
+	    public override void SetType() {
+			
+	        ownHookColor = HookColors.GREEN;
+	        base.SetType();
+
+	    }
+
+	}
 
 }
