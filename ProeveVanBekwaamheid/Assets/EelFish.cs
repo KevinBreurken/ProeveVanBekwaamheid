0419271857c51e486998374f96b58b7fdb22fc15
Chanisco Tromp <Chanisco@live.nl>
2016-05-27 12:31:45 +0200

    Created Eel Fish (Start)

---
+++ ProeveVanBekwaamheid/Assets/EelFish.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Base.Game.Hooks;
+
+namespace Base.Game.Fish {
+
+    public class EelFish:  FishBehaviour{
+        public void Start() {
+
+            speed = Random.Range(20,30);
+            Init();
+
+        }
+
+        public void OnEnable() {
+
+            speed = Random.Range(20,30);
+            InMotion = true;
+
+        }
+
+        void Update() {
+
+            SwimDirection(ownDirection);
+            OutOfBound();
+
+        }
+
+        public override void RespondToHook(HookBehaviour _target) {
+            base.RespondToHook(_target);
+
+        }
+
+
+    }
+}
