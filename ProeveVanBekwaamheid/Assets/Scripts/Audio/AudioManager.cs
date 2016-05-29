291c70971c0db6fb8372ad5f9965bbf8360d1a09
KevinBreurken <kevinbreurken@hotmail.com>
2016-05-29 22:10:09 +0200

    Fixed WebGL Audio

--- ProeveVanBekwaamheid/Assets/Scripts/Audio/AudioManager.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Audio/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
+using DG.Tweening;
 
 namespace Base.Audio {
 
@@ -8,17 +9,31 @@ namespace Base.Audio {
 
         public AudioMixerSnapshot defaultSnapshot;
         public AudioMixerSnapshot underwaterSnapshot;
-	
+        private AudioSource outsideSound;
+
+        private bool isWebGL = false;
+
+        void Awake () {
+            outsideSound = transform.FindChild("Ambience_AboveWater").GetComponent<AudioSource>();
+            #if UNITY_WEBGL
+            isWebGL = true;
+            #endif
+        }
+
         public void SetUnderwaterMixing (float _time) {
 
-            underwaterSnapshot.TransitionTo(_time);
-
+            if (!isWebGL)
+                underwaterSnapshot.TransitionTo(_time);
+            else
+                outsideSound.DOFade(0, 1);
         }
 
         public void SetAboveWaterMixing (float _time) {
 
-            defaultSnapshot.TransitionTo(_time);
-
+            if (!isWebGL)
+                defaultSnapshot.TransitionTo(_time);
+            else
+                outsideSound.DOFade(1, 1);
         }
 			
     }
