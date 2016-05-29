a23672c0011703197e17c754748af78ae630c8f0
Chanisco Tromp <Chanisco@live.nl>
2016-05-29 18:11:14 +0100

    Made Editor for SetFishInSequence script

---
+++ ProeveVanBekwaamheid/Assets/Scripts/Fish/Support/Editor/SetFishInSequenceEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using Base.Game.Fish;
+
+
+[CustomEditor(typeof(SetFishInSequence))]
+public class SetFishInSequenceEditor: Editor {
+
+    public override void OnInspectorGUI() {
+
+        SetFishInSequence myTarget = (SetFishInSequence)target;
+        if (GUILayout.Button("Set Sequence")) {
+            Debug.Log("[Sequence Set]");
+            myTarget.SetFunctionality();
+
+        }
+    }
+}
